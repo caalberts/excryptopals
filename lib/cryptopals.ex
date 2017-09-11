7d187580f8bccda609a54665d3f234051672c5e3
Albert Salim <albertlimca@gmail.com>
2017-09-11 21:11:56 +0800

    Set 1 Challenge 1 - converts hex encoded string to base64 encoded string

--- lib/cryptopals.ex
+++ lib/cryptopals.ex
@@ -1,18 +1,17 @@
 defmodule Cryptopals do
-  @moduledoc """
-  Documentation for Cryptopals.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cryptopals.hello
-      :world
-
-  """
-  def hello do
-    :world
+  def hex_to_base64(hex) do
+    hex
+    |> from_hex
+    |> to_base64
   end
+
+  defp from_hex(hex) do
+    Base.decode16!(hex, case: :lower)
+  end
+
+  defp to_base64(bin) do
+    Base.encode64(bin)
+  end
+
 end
--- test/cryptopals_test.exs
+++ test/cryptopals_test.exs
@@ -2,7 +2,9 @@ defmodule CryptopalsTest do
   use ExUnit.Case
   doctest Cryptopals
 
-  test "greets the world" do
-    assert Cryptopals.hello() == :world
+  test "convert hex to base 64" do
+    hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+
+    assert "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t" = Cryptopals.hex_to_base64(hex)
   end
 end
